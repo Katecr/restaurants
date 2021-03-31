e62ac8629a0ea7f6ff446429568e77302a8108ca
kate <katecr17@gmail.com>
2021-03-31 13:22:41 -0500

    Placing pagination to carousel

--- screens/restaurants/Restaurant.js
+++ screens/restaurants/Restaurant.js
@@ -13,6 +13,7 @@ const widthScreen = Dimensions.get("window").width
 export default function Restaurant({ navigation, route }) {
     const { id, name } = route.params
     const [restaurant, setRestaurant] = useState(null)
+    const [activeSlide, setActiveSlide] = useState(0)
 
     navigation.setOptions({title: name})
 
@@ -37,6 +38,8 @@ export default function Restaurant({ navigation, route }) {
                 images={restaurant.images}
                 height={250}
                 width={widthScreen}
+                activeSlide={activeSlide}
+                setActiveSlide={setActiveSlide}
             />
             <Text>{restaurant.description}</Text>
         </ScrollView>
