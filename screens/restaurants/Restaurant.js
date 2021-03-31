1cefd9b140414e783dd9037e33fdb2091c73a120
kate <katecr17@gmail.com>
2021-03-30 23:33:19 -0500

    Obtaining restaurant information

--- screens/restaurants/Restaurant.js
+++ screens/restaurants/Restaurant.js
@@ -1,13 +1,34 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Alert, StyleSheet, Text, View } from 'react-native'
+
+
+import { getDocumentById } from '../../utils/actions'
+import Loading from '../../components/Loading'
 
 export default function Restaurant({ navigation, route }) {
     const { id, name } = route.params
+    const [restaurant, setRestaurant] = useState(null)
 
     navigation.setOptions({title: name})
+
+    useEffect(() => {
+        (async() => {
+            const response = await getDocumentById("restaurants", id)
+            if(response.statusResponse){
+                setRestaurant(response.document)
+            }else{
+                setRestaurant({})
+                Alert.alert("Ocurrio un problema cargando el restaurante, intente más tarde.")
+            }
+        })()
+    }, [])
+
+    if(!restaurant){
+        return <Loading isVisible={true} text={"Cargando..."}/>
+    }
     return (
         <View>
-            <Text>Restaurant...</Text>
+            <Text>{restaurant.description}</Text>
         </View>
     )
 }
