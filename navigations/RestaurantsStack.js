fd843792c846448f463ec7e79bf0e2fa76122e5a
kate <katecr17@gmail.com>
2021-04-01 15:12:17 -0500

    Starting with the add comments screen

--- navigations/RestaurantsStack.js
+++ navigations/RestaurantsStack.js
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack'
 import Restaurants from '../screens/restaurants/Restaurants'
 import AddRestaurant from '../screens/restaurants/AddRestaurant'
 import Restaurant from '../screens/restaurants/Restaurant'
+import AddReviewRestaurant from '../screens/restaurants/AddReviewRestaurant'
 
 const Stack = createStackNavigator()
 
@@ -24,7 +25,11 @@ export default function RestaurantsStack() {
                 name="restaurant"
                 component={Restaurant}      
            />
-           
+           <Stack.Screen
+                name="add-review-restaurant"
+                component={AddReviewRestaurant}    
+                options={{title: "Nuevo comentario"}}   
+           />           
        </Stack.Navigator> 
     )
 }
