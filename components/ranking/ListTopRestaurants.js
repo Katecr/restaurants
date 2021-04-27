6b9d3ace0cbe082df4edb9bb0aaa8b4dfce549a7
kate <katecr17@gmail.com>
2021-04-27 08:16:41 -0500

    getting the top rating list

---
+++ components/ranking/ListTopRestaurants.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function ListTopRestaurants() {
+    return (
+        <View>
+            <Text>ListTopRestaurants...</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
