d8700c544e320d8aec3c6e60dc173b3518a91b00
kate <katecr17@gmail.com>
2021-05-04 08:44:03 -0500

    Adding method to call, send email and write to WhatsApp

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,6 +3,7 @@ import * as ImagePicker from 'expo-image-picker'
 import * as Location from 'expo-location'
 import { Alert } from 'react-native'
 import { size } from 'lodash';
+import { Linking } from 'react-native';
 
 export function validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -59,4 +60,19 @@ export const formatPhone = (callingCode, phone) => {
         return `+(${callingCode}) ${phone}`
     }
     return `+(${callingCode}) ${phone.substr(0, 3)} ${phone.substr(3, 3)} ${phone.substr(6, 4)}`
+}
+
+export const callNumber = (phoneNumber) => {
+    Linking.openURL(`tel:&{phoneNumber}`)
+}
+
+export const sendWhatsApp = (phoneNumber, text) => {
+    const link = `https://wa.me/${phoneNumber}?text=${text}`
+    Linking.canOpenURL(link).then((supported) => {
+        if(!supported){
+            Alert.alert("Por favor instale WhatsApp para enviar un mensaje directo.")
+            return
+        }
+        return Linking.openURL(link)
+    })
 }
