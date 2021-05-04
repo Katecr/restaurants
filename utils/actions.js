493e9e445797c1d1bfed80cd742a9480349ff2b4
kate <katecr17@gmail.com>
2021-05-04 10:57:16 -0500

    Notifications I

--- utils/actions.js
+++ utils/actions.js
@@ -2,6 +2,8 @@ import { firebaseApp } from './firebase'
 import * as firebase from 'firebase'
 import { FireSQL } from 'firesql'
 import 'firebase/firestore'
+import * as Notifications from 'expo-notifications'
+import Constans from 'expo-constants'
 
 
 import { fileToBlob } from './helpers'
@@ -295,4 +297,36 @@ export const searchRestaurants = async(criteria) => {
         result.error = error
     }
     return result     
+}
+
+export const getToken = async() => {
+    if (!Constans.isDevice) {
+        Alert.alert("Debes utilizar un dispositivo físico para poder utilizar las notificaciones.")
+        return
+    }
+
+    const { status: existingStatus } = await Notifications.getPermissionsAsync()
+    let finalStatus = existingStatus
+    if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync()
+        finalStatus = status 
+    }
+
+    if (finalStatus !== "granted") {
+        Alert.alert("Debes dar permiso para acceder a las notificaciones.")
+        return
+    }
+
+    const token = (await Notifications.getExpoPushTokenAsync()).data
+
+    if (Platform.OS == "android") {
+        Notifications.setNotificationChannelAsync("default", {
+            name: "default",
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: "#FF231F7C"
+        })
+    }
+
+    return token
 }
